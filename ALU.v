f86024dab373c7f3a227cbffa800d2f70055e51c
Owen Fazzini <161644496+sulvento@users.noreply.github.com>
2024-03-09 12:14:34 -0500

    Update ALU.v
    added logic cases to switch statement

--- ALU.v
+++ ALU.v
@@ -18,20 +18,41 @@ output result
 );
 
 case(instruction) 
-    {NOT}: begin
+
+    //logic cases
+    {16'h0x1000}: begin //NOT
+        result = ~a;
+    end
+    {16'h0x1002}: begin //OR
+        result = a | b;
+    end
+    {16'h0x1001}: begin //AND
+        result = a & b;
+    end
+    {16'h0x1003}: begin //XOR
+        result = a ^ b;
+    end
+
+    //bitshifts
+    {16'h0x0010}: begin //shift right
+        result = a >> b;
+    end
+    {16'h0x0011}: begin //shift left
+        result = a << b;
+    end
+    {16'h0x0012}: begin //rotate right
 
     end
-    {OR}: begin
+    {16'h0x0013}: begin //rotate left
 
     end
-    {AND}: begin
-
-    end
-    {XOR}: begin
+    {16'h0x0014}: begin //swap
 
     end
 
-    //Finish switch statement, implement logic
+    
+
+
 
 
 endcase
