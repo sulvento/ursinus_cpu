f18be9ccabd58939e2c91d715293e3658d4d26a0
Owen Fazzini <161644496+sulvento@users.noreply.github.com>
2024-03-18 09:34:05 -0400

    Update ALU.v
    This is a basic outline of the ALU. NO TESTING HAS BEEN DONE YET! This does not include any program flow instructions as well as that will have to come with the rest of the cpu implementation. Revisions will come with testing

--- ALU.v
+++ ALU.v
@@ -8,12 +8,15 @@ CS-274
 
 //This ALU is INCOMPLETE: it is a self-contained unit, with no integration with the wider CPU.
 
+//Switch statement for every state (fetch, decode, execute, writeback, etc.)
+
 
 module ALU(
 input [19:0] instruction, //name of the instruction called, control signal
 input [19:0] A, //register A 
 input [19:0] B, //register B
 output [19:0] result //output reg
+output carry_out; //for adding/subtracting with carry
 );
 
 case(instruction) 
@@ -60,38 +63,64 @@ case(instruction)
         result = a + b;
     end
     {20'h0x0017D}: begin //ADDC 
-
+        integer carry_in;
+        result = a + b + carry_in;
+        carry_out = (result < a) || (result < b);
     end
     {20'h0x00191}: begin //SUB
-
+        result = a - b;
     end
     {20'h0x001A5}: begin //SUBC
-
+        integer borrow_in;
+        result = a - b - borrow_in;
+        carry_out = (a < b) || ((a==b) && !borrow_in);
     end
 
     //equality
     {20'h0x001B9}: begin //EQ
-        
+        if a = b begin
+            Z = 1;
+        end
+        else begin
+            Z = 0;
+        end
     end
     {20'h0x001CD}: begin //GT
-
+        if a > b begin
+            N = 1;
+        end
+        else begin
+            N = 0;
+        end
     end
     {20'h0x001E1}: begin //LT
-
+        if a < b begin
+            N = 1;
+        end
+        else begin
+            N = 0;
+        end
     end
     {20'h0x001F5}: begin //GET
-
+        if a >= b begin
+            Z = 1;
+            N = 0;
+        end
+        else begin
+            Z = 0;
+            N = 1;
+        end
     end
     {20'h0x00209}: begin //LET
-
+        if a <= b begin
+            Z = 1;
+            N = 1;
+        end
+        else begin
+            Z = 0;
+            N = 0;
+        end
     end
-
-
-
-    
-
-
-
 
 endcase
 
