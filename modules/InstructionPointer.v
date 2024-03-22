e9c48574b106498fc448d57b44791104fff7b986
Owen Fazzini <161644496+sulvento@users.noreply.github.com>
2024-03-22 15:02:36 -0400

    Update InstructionPointer.v

--- modules/InstructionPointer.v
+++ modules/InstructionPointer.v
@@ -1,19 +1,18 @@
-module instruction_pointer (
-    input clk,
-    input reset,
-    input load_ip,
-    input inc_ip,  
-    input [15:0] ip_data_in,  
-    output reg [15:0] ip_data_out 
+module ipreg (                      //instruction pointer register
+    input clk, reset,
+    input inc,
+    input [19:0] data_in,  
+    output reg [19:0] data_out 
 );
+
+//this register contains the offset added to the instruction segment register
+//to find instructions in memory.
 
 always @(posedge clk or posedge reset) begin
     if (reset) begin
-        ip_data_out <= 16'd0; 
-    end else if (load_ip) begin
-        ip_data_out <= ip_data_in;
-    end else if (inc_ip) begin
-        ip_data_out <= ip_data_out + 16'd1; 
+        data_out <= 20'b0; 
+    end else if (inc) begin
+        data_out <= data_out + 1;           //increments pointer by 1 
     end
 end
 
