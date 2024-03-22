55b94f45f9380e4d25466198878fb052ef9afdaf
Owen Fazzini <161644496+sulvento@users.noreply.github.com>
2024-03-22 12:43:28 -0400

    Update StaticSegmentRegister_tb.v

--- testbenches/StaticSegmentRegister_tb.v
+++ testbenches/StaticSegmentRegister_tb.v
@@ -1,61 +1,36 @@
-`timescale 1ns/1ns
-`include "StaticSegmentRegister.v"
+`include "statseg.v"
 
-module static_segment_register_tb;
+module testbench_statseg;
+    reg clk = 0;
+    reg reset, loadsig;
+    reg [19:0] data_in;
+    wire [19:0] data_out;
 
-  reg clk;
-  reg reset;
-  reg load_ssr;
-  reg [15:0] ssr_data_in;
-  wire [15:0] ssr_data_out;
-
-
-  static_segment_register dut (
+statseg SS(
     .clk(clk),
     .reset(reset),
-    .load_ssr(load_ssr),
-    .ssr_data_in(ssr_data_in),
-    .ssr_data_out(ssr_data_out)
-  );
+    .loadsig(loadsig),
+    .data_in(data_in),
+    .data_out(data_out)
+);
 
+always #5 clk = ~clk;
 
-  initial begin
-    clk = 0;
-    forever #5 clk = ~clk; 
-  end
-
-
-  initial begin
-
-    $dumpfile("static_segment_register_tb.vcd");
-    $dumpvars(0, static_segment_register_tb);
+initial begin
+    $dumpfile("testbench_statseg.vcd");
+    $dumpvars(1,SS);
 
     reset = 1;
-    load_ssr = 0;
-    #10; 
-    reset = 0; 
+    #5;
+    reset = 0;
+    loadsig = 1;
+    data_in = 20'hAD1;
+    #5;
+
+    $finish;
+
+end
 
 
-    load_ssr = 1;
-    ssr_data_in = 16'h2000; 
-    #10; 
-    load_ssr = 0;
 
-
-    ssr_data_in = 16'h3500; 
-    load_ssr = 1;
-    #10;
-    load_ssr = 0;
-
-    #50;
-
-    $finish; 
-  end
-
-
-  always @(posedge clk) begin
-    $display("Time: %d, clk: %b, reset: %b, load_ssr: %b, ssr_data_in: %h, ssr_data_out: %h",
-              $time, clk, reset, load_ssr, ssr_data_in, ssr_data_out); 
-  end
-
-endmodule 
+endmodule
