d4063cdcb4f46a3662e862d306a0f3cd84d14855
Owen Fazzini <161644496+sulvento@users.noreply.github.com>
2024-03-22 19:31:16 -0400

    Update DynamicPointer_tb.v
    simple testbench, demonstrating 3 functionalities of dynamicpointer (read, write, increment)

--- testbenches/DynamicPointer_tb.v
+++ testbenches/DynamicPointer_tb.v
@@ -1,72 +1,49 @@
-`timescale 1ns/1ns
-`include "DynamicPointer.v"
+`include "dpreg.v"
 
-module dynamic_pointer_tb;
+module testbench_dpreg;
 
-  reg clk;
-  reg reset;
-  reg load_dp;
-  reg inc_dp;
-  reg write_dp;
-  reg [15:0] dp_data_in;
-  wire [15:0] dp_data_out;
+reg clk = 0;
+reg reset, inc, writesig, readsig;
+reg [19:0] data_in;
+wire [19:0] data_out;
 
-  dynamic_pointer dut (
+dynamic_pointer dpreg(
     .clk(clk),
     .reset(reset),
-    .load_dp(load_dp),
-    .inc_dp(inc_dp),
-    .write_dp(write_dp),
-    .dp_data_in(dp_data_in),
-    .dp_data_out(dp_data_out)
-  );
+    .inc(inc),
+    .writesig(writesig),
+    .readsig(readsig),
+    .data_in(data_in),
+    .data_out(data_out)
+);
 
-  initial begin
-    clk = 0;
-    forever #5 clk = ~clk;
-  end
+always #5 clk = ~clk;
 
-  initial begin
+initial begin
+    $dumpfile("testbench_dpreg.vcd");
+    $dumpvars(1,dpreg);
 
-    $dumpfile("dynamic_pointer_tb.vcd");
-    $dumpvars(0, dynamic_pointer_tb);
-
-    
     reset = 1;
-    #10;
+    #5;
     reset = 0;
+    writesig = 1;
+    data_in = 20'b0;
+    #5;
+    writesig = 0;
+    readsig = 1;
+    #5; 
+    readsig = 0;
+    inc = 1;
+    #5;
+    readsig = 1;
+    #20;
+    $finish;
 
-    load_dp = 1;
-    dp_data_in = 16'h3800; 
-    #10;
-    load_dp = 0;
 
-    inc_dp = 1;
-    #10;
-    inc_dp = 0;
-    #10;
-    inc_dp = 1;
-    #10;
-    inc_dp = 0;
 
-    write_dp = 1;
-    dp_data_in = 16'h5000;
-    #10;
-    write_dp = 0;
+end
 
-    #10;
-    inc_dp = 1;
-    #10; 
-    inc_dp = 0; 
 
-    #30;  
-    
-    $finish; 
-  end
 
-  always @(posedge clk) begin
-    $display("Time: %d, clk: %b, reset: %b, load_dp: %b, inc_dp: %b, write_dp: %b, dp_data_in: %h, dp_data_out: %h",
-              $time, clk, reset, load_dp, inc_dp, write_dp, dp_data_in, dp_data_out); 
-  end
 
-endmodule 
+endmodule
